3db281787833687aa6afd4361229a8bd58e7fcd2
Saleha Irum <117927283+salehairum@users.noreply.github.com>
2026-04-16 23:11:14 +0500

    envconfig

--- frontend/env-config.template.js
+++ frontend/env-config.template.js
@@ -1,7 +1,3 @@
 window.APP_CONFIG = Object.freeze({
-    USER_SERVICE_URL: "/api/user-services",
-    BOOKING_SERVICE_URL: "http://localhost:5000",
-    NOTIFICATION_SERVICE_URL: "http://localhost:5001",
-    EVENT_SERVICE_URL: "http://localhost:8080",
-    PAYMENT_SERVICE_URL: "http://localhost:5002"
+    USER_SERVICE_URL: "http://localhost:8000",
 });
